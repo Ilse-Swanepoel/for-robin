19b31ad1d49da4e25f6db3e041cc2bcb99aa3ecc
Ilse Swanepoel <ilseswanepoel@users-MacBook-Pro.local>
2023-08-15 10:33:21 +1200

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/hello")
+def printhelloWorld():
+    html_content = "<h1>Hello, World</h1>"
+    return HTMLResponse(content=html_content)
+
+@app.get("/ahh")
+def printAhh():
+    html_content = "<h1>AAAAAAAAAAAHHHHHH!!!</h1>"
+    return HTMLResponse(content=html_content)
+
+@app.get("/slay")
+def printSlay():
+    html_content = "<h1>SLAY QUEEN!</h1>"
+    return HTMLResponse(content=html_content)
